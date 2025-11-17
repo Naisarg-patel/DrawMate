e2138a9be7eb42f5e5cf7c757bdeec0bfc9850cb
23012011066 <23012011066@gnu.ac.in>
2025-11-17 20:55:21 +0530

    Update BrushLibrary.kt

--- app/src/main/java/com/example/DrawMate/BrushLibrary.kt
+++ app/src/main/java/com/example/DrawMate/BrushLibrary.kt
@@ -12,78 +12,14 @@ object BrushLibrary {
         BrushType("Marker", R.drawable.marker, Color.BLACK, 16f, Paint.Cap.BUTT, category = "basic", opacity = 150),
         BrushType("soft brush", R.drawable.softbruush, Color.BLACK, 15f, Paint.Cap.ROUND, category = "basic", opacity = 100),
         BrushType("Hard brush", R.drawable.hardbrush, Color.BLACK, 40f, Paint.Cap.ROUND, category = "basic", opacity = 255),
-        BrushType(
-            "half tone star",
-            R.drawable.halftonestart,
-            Color.BLACK,
-            100f,
-            Paint.Cap.ROUND,
-            textureRes = R.drawable.halftonestart,
-            category = "halftone"
-        ),
-        BrushType(
-            "half tone hexagon",
-            R.drawable.halftonehexagon,
-            Color.BLACK,
-            100f,
-            Paint.Cap.ROUND,
-            textureRes = R.drawable.halftonehexagon,
-            category = "halftone"
-        ),
-        BrushType(
-            "crosshatch",
-            R.drawable.cross,
-            Color.BLACK,
-            100f,
-            Paint.Cap.ROUND,
-            textureRes = R.drawable.cross,
-            category = "halftone"
-        ),
-        BrushType(
-            " Half tone dot inv 1",
-            R.drawable.dot1,
-            Color.BLACK,
-            100f,
-            Paint.Cap.ROUND,
-            textureRes = R.drawable.dot1,
-            category = "halftone"
-        ),
-        BrushType(
-            "half tone dot inv 2 ",
-            R.drawable.dot2,
-            Color.BLACK,
-            15f,
-            Paint.Cap.ROUND,
-            textureRes = R.drawable.dot2,
-            category = "halftone"
-        ),
-        BrushType(
-            "half tone dot 1",
-            R.drawable.dotpat1,
-            Color.BLACK,
-            100f,
-            Paint.Cap.ROUND,
-            textureRes = R.drawable.dotpat1,
-            category = "halftone"
-        ),
-        BrushType(
-            "half tone dot 2",
-            R.drawable.dotpat2,
-            Color.BLACK,
-            100f,
-            Paint.Cap.ROUND,
-            textureRes = R.drawable.dotpat2,
-            category = "halftone"
-        ),
-        BrushType(
-            "half tone dot 3",
-            R.drawable.dotpat3,
-            Color.BLACK,
-            100f,
-            Paint.Cap.ROUND,
-            textureRes = R.drawable.dotpat3,
-            category = "halftone"
-        ),
+        BrushType("half tone star",R.drawable.halftonestart,Color.BLACK,100f,Paint.Cap.ROUND,textureRes = R.drawable.halftonestart,category = "halftone"),
+        BrushType("half tone hexagon",R.drawable.halftonehexagon,Color.BLACK,100f,Paint.Cap.ROUND,textureRes = R.drawable.halftonehexagon,category = "halftone"),
+        BrushType("crosshatch",R.drawable.cross,Color.BLACK,100f,Paint.Cap.ROUND,textureRes = R.drawable.cross,category = "halftone"),
+        BrushType(" Half tone dot inv 1",R.drawable.dot1,Color.BLACK,100f,Paint.Cap.ROUND,textureRes = R.drawable.dot1,category = "halftone"),
+        BrushType("half tone dot inv 2 ",R.drawable.dot2,Color.BLACK,15f,Paint.Cap.ROUND,textureRes = R.drawable.dot2,category = "halftone"),
+        BrushType("half tone dot 1",R.drawable.dotpat1,Color.BLACK,100f,Paint.Cap.ROUND,textureRes = R.drawable.dotpat1,category = "halftone"),
+        BrushType("half tone dot 2",R.drawable.dotpat2,Color.BLACK,100f,Paint.Cap.ROUND,textureRes = R.drawable.dotpat2,category = "halftone"),
+        BrushType("half tone dot 3",R.drawable.dotpat3,Color.BLACK,100f,Paint.Cap.ROUND,textureRes = R.drawable.dotpat3,category = "halftone"),
         BrushType("half tone dot 4",R.drawable.dotpat4, Color.BLACK, 100f, Paint.Cap.ROUND, textureRes = R.drawable.dotpat4, category = "halftone"),
         BrushType("half tone grid 1",R.drawable.grid, Color.BLACK, 100f, Paint.Cap.ROUND, textureRes = R.drawable.grid, category = "halftone"),
         BrushType("half tone grid 2",R.drawable.grid1, Color.BLACK, 100f, Paint.Cap.ROUND, textureRes = R.drawable.grid1, category = "halftone"),
